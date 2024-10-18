3b0564780205182e7aee0e41d2ddcd99630568a7
YOUR_NAME_HERE <YOUR_EMAIL_ADDRESS_HERE>
2024-10-18 16:44:12 +0800

    Refactor getInitialConf code

--- getInitialConf/getInitialConf.go
+++ getInitialConf/getInitialConf.go
@@ -3,20 +3,77 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
-	"io/fs"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
 )
 
-type inputFile struct {
-	Path   []string
-	Filter string
-}
+var (
+	fileList = []string{
+		"/usr/bin/perl",
+		"/usr/bin/x2goagent",
+		"/usr/bin/x2gobasepath",
+		"/usr/bin/x2gocmdexitmessage",
+		"/usr/bin/x2gofeature",
+		"/usr/bin/x2gofeaturelist",
+		"/usr/bin/x2gofm",
+		"/usr/bin/x2gogetapps",
+		"/usr/bin/x2gogetservers",
+		"/usr/bin/x2gokdrive",
+		"/usr/bin/x2gokdriveclient",
+		"/usr/bin/x2golistdesktops",
+		"/usr/bin/x2golistmounts",
+		"/usr/bin/x2golistsessions",
+		"/usr/bin/x2golistshadowsessions",
+		"/usr/bin/x2gomountdirs",
+		"/usr/bin/x2gooptionsstring",
+		"/usr/bin/x2gopath",
+		"/usr/bin/x2goprint",
+		"/usr/bin/x2goresume-session",
+		"/usr/bin/x2goruncommand",
+		"/usr/bin/x2goserver-run-extensions",
+		"/usr/bin/x2gosessionlimit",
+		"/usr/bin/x2gosetkeyboard",
+		"/usr/bin/x2gostartagent",
+		"/usr/bin/x2gosuspend-session",
+		"/usr/bin/x2goterminate-session",
+		"/usr/bin/x2goumount-session",
+		"/usr/bin/x2goversion",
+		"/etc/x2go/Xsession",
+		"/etc/x2go/Xsession.options",
+		"/etc/x2go/x2go_logout",
+		"/etc/x2go/x2goagent.options",
+		"/etc/x2go/x2gokdrive.options",
+		"/etc/x2go/x2goserver.conf",
+		"/etc/x2go/x2gosql/sql",
+		"/usr/lib/systemd/system/x2goserver.service",
+		"/etc/systemd/system/multi-user.target.wants/x2goserver.service",
+		"/usr/sbin/sshd",
+		"/usr/lib/systemd/system/ssh.service",
+		"/etc/systemd/system/multi-user.target.wants/ssh.service",
+		"/usr/lib/systemd/system/ssh.socket",
+		"/etc/ssh/sshd_config",
+		"/usr/lib/systemd/system/watchdog.service",
+		"/etc/systemd/system/multi-user.target.wants/watchdog.service",
+	}
+	dirList = []string{
+		"/etc/x2go",
+		"/etc/x2go/Xresources",
+		"/etc/x2go/Xsession.d",
+		"/etc/x2go/Xsession.options.d",
+		"/etc/x2go/x2go_logout.d",
+		"/etc/x2go/x2gosql",
+		"/etc/x2go/x2gosql/passwords",
+	}
+	procList = []string{
+		"/run/x2goserver.pid",
+		"/run/sshd.pid",
+	}
+)
 
 type config struct {
 	Data   []fileConf
@@ -28,8 +85,6 @@ type fileConf struct {
 	Hash []string
 }
 
-var data []fileConf
-
 func getFileSHA256(path string) (string, error) {
 	file, err := os.ReadFile(path)
 	if err != nil {
@@ -39,95 +94,101 @@ func getFileSHA256(path string) (string, error) {
 	return hex.EncodeToString(fileSHA[:]), nil
 }
 
-func getFileConf(file string) {
-	info, err := os.Stat(file)
-	if err != nil {
-		log.Printf("Access %v error: %v\n", file, err)
-		return
-	}
-	if info.IsDir() {
-		log.Printf("%v is a directory!\n", file)
-		return
-	}
-	path, err := filepath.Abs(file)
-	if err != nil {
-		log.Printf("Get %v config failed: %v\n", file, err)
-		return
-	}
-	fileHash, err := getFileSHA256(path)
-	if err != nil {
-		log.Printf("Get %v hash failed: %v\n", file, err)
-		return
-	}
-	data = append(data, fileConf{Path: path, Hash: []string{fileHash}})
-}
-
-func getDirConfig(dir string, filter string) {
-	info, err := os.Stat(dir)
-	if err != nil {
-		log.Printf("Access %v error: %v", dir, err)
-		return
-	}
-	if !info.IsDir() {
-		log.Printf("%v is not a directory!", dir)
-		return
-	}
-	filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+func getConfig(inputPath, outputPath string) {
+	var newConf, oldConf config
+	if inputPath != "" {
+		inputByte, err := os.ReadFile(inputPath)
 		if err != nil {
-			log.Printf("Access %v error: %v", path, err)
-			return nil
+			log.Printf("Access %v error: %v\n", inputPath, err)
+			os.Exit(-1)
 		}
-		if !strings.Contains(path, filter) {
-			return nil
+		yaml.Unmarshal(inputByte, &oldConf)
+	}
+
+	var conf = make(map[string]map[string]struct{})
+	for _, oldFileConf := range oldConf.Data {
+		conf[oldFileConf.Path] = make(map[string]struct{})
+		for _, hash := range oldFileConf.Hash {
+			conf[oldFileConf.Path][hash] = struct{}{}
 		}
-		path, err = filepath.Abs(path)
+	}
+	for _, iniPath := range fileList {
+		absPath, err := filepath.Abs(iniPath)
 		if err != nil {
-			log.Printf("Get %v config failed: %v", path, err)
-			return nil
+			log.Printf("Get file %v abs path err: %v\n", iniPath, err)
+			continue
 		}
-		if info.IsDir() {
-			data = append(data, fileConf{Path: path, Hash: []string{}})
-		} else {
-			fileHash, err := getFileSHA256(path)
-			if err != nil {
-				log.Printf("Get %v hash failed: %v", path, err)
-				return nil
-			}
-			data = append(data, fileConf{Path: path, Hash: []string{fileHash}})
+		hash, err := getFileSHA256(absPath)
+		if err != nil {
+			log.Printf("Access file %v err: %v\n", iniPath, err)
+			continue
 		}
-		return nil
-	})
-}
+		if _, exist := conf[absPath]; !exist {
+			conf[absPath] = make(map[string]struct{})
+		}
+		conf[absPath][hash] = struct{}{}
+	}
+	for _, iniPath := range dirList {
+		absPath, err := filepath.Abs(iniPath)
+		if err != nil {
+			log.Printf("Get dir %v abs path err: %v\n", iniPath, err)
+			continue
+		}
+		conf[absPath] = map[string]struct{}{}
+	}
+	for _, pidFilePath := range procList {
+		absPidPath, err := filepath.Abs(pidFilePath)
+		if err != nil {
+			log.Printf("Get process pid file %v abs path err: %v\n", pidFilePath, err)
+			continue
+		}
+		pidFile, err := os.Open(absPidPath)
+		if err != nil {
+			log.Printf("Access process pid file %v err: %v\n", pidFilePath, err)
+			continue
+		}
+		var pid int
+		_, err = fmt.Fscanf(pidFile, "%d", &pid)
+		if err != nil {
+			log.Printf("Read pid from file %v err: %v\n", pidFilePath, err)
+			continue
+		}
+		exePath, err := os.Readlink(fmt.Sprintf("/proc/%d/exe", pid))
+		if err != nil {
+			log.Printf("Access real exe file of %v err: %v\n", pidFilePath, err)
+			continue
+		}
+		absExePath, err := filepath.Abs(exePath)
+		if err != nil {
+			log.Printf("Get exe file %v abs path err: %v\n", exePath, err)
+			continue
+		}
+		hash, err := getFileSHA256(absExePath)
+		if err != nil {
+			log.Printf("Access exe file %v err: %v\n", absExePath, err)
+			continue
+		}
+		if _, exist := conf[absPidPath]; !exist {
+			conf[absPidPath] = make(map[string]struct{})
+		}
+		conf[absPidPath][hash] = struct{}{}
+	}
 
-func getConfig(path string, filter string) {
-	info, err := os.Stat(path)
-	if err != nil {
-		log.Printf("Access %v error: %v", path, err)
-		return
+	for path, hashs := range conf {
+		var newFileConf fileConf
+		newFileConf.Path = path
+		for hash := range hashs {
+			newFileConf.Hash = append(newFileConf.Hash, hash)
+		}
+		newConf.Data = append(newConf.Data, newFileConf)
 	}
-	if info.IsDir() {
-		getDirConfig(path, filter)
-	} else {
-		getFileConf(path)
-	}
-}
-
-func createConfFile(data []fileConf, path string) {
-	var conf = config{
-		Data:   data,
-		DryRun: true,
-	}
-	dataBytes, err := yaml.Marshal(conf)
+	newConf.DryRun = false
+	outputByte, err := yaml.Marshal(newConf)
 	if err != nil {
 		log.Printf("Marshal config err: %v", err)
 		os.Exit(-1)
 	}
-	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
-	if err != nil {
-		log.Printf("Open config file err: %v", err)
-		os.Exit(-1)
-	}
-	_, err = file.Write(dataBytes)
+	err = os.WriteFile(outputPath, outputByte, 0644)
 	if err != nil {
 		log.Printf("Write config file err: %v", err)
 		os.Exit(-1)
@@ -135,38 +196,16 @@ func createConfFile(data []fileConf, path string) {
 }
 
 func main() {
-	var filter string
-	var ouputFile string
-	var configFile string
+	var inputPath, outputPath string
 	var rootCmd = &cobra.Command{
 		Use:   "getInitialConf",
-		Short: "Get initial unsigned config for watchdog, only used to get file config",
+		Short: "Get initial unsigned config for watchdog",
 		Run: func(cmd *cobra.Command, args []string) {
-			if cmd.Flags().Changed("input") {
-				var inputs []inputFile
-				inputByte, err := os.ReadFile(configFile)
-				if err != nil {
-					log.Printf("Access %v error: %v\n", configFile, err)
-					os.Exit(-1)
-				}
-				yaml.Unmarshal(inputByte, &inputs)
-				for _, input := range inputs {
-					for _, path := range input.Path {
-						getConfig(path, input.Filter)
-					}
-				}
-			} else {
-				for _, arg := range args {
-					getConfig(arg, filter)
-				}
-			}
-			createConfFile(data, ouputFile)
+			getConfig(inputPath, outputPath)
 		},
 	}
-	rootCmd.PersistentFlags().StringVarP(&ouputFile, "output", "o", "watchdog.yaml", "Specify the output file")
-	rootCmd.PersistentFlags().StringVarP(&filter, "filter", "f", "", "Filter files by name, will not take effect on the specified file")
-	rootCmd.PersistentFlags().StringVarP(&configFile, "input", "i", "", "Use a file as input to specify file, directory and filter, other specified file, directory or filter will be ignored")
-
+	rootCmd.Flags().StringVarP(&inputPath, "input", "i", "", "Specify input file path, usually the old config file")
+	rootCmd.Flags().StringVarP(&outputPath, "output", "o", "watchdog.yaml", "Specify output file path")
 	var completion = &cobra.Command{
 		Use: "completion",
 	}
