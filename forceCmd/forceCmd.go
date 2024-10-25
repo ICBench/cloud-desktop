57e60df9d748d7553505250c552df2e60e4761ce
Zsz-icbench <shizhao@icbench.com>
2024-10-25 14:34:17 +0800

    ssh force command (#10)
    * sets x2gokdrive a higher priority
    * adds logs
    * for external commands, checks their abspath
    * allows more commands

--- forceCmd/forceCmd.go
+++ forceCmd/forceCmd.go
@@ -5,29 +5,36 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"syscall"
 
+	"github.com/coreos/go-systemd/v22/journal"
 	"github.com/mattn/go-shellwords"
 	"mvdan.cc/sh/v3/syntax"
 )
 
 var home string
 
-func checkCmd(cmd string) bool {
+func checkCmd(cmd string) (cmdList []string, allow bool) {
+	allow = true
 	_, args, err := shellwords.ParseWithEnvs(cmd)
 	if err != nil {
-		return false
+		allow = false
+		return
 	}
 	path, err := exec.LookPath(args[0])
 	if err != nil {
-		return true
+		allow = true
+		return
 	}
 	path, err = filepath.Abs(path)
 	if err != nil {
 		log.Printf("Parse path error: %v\n", err)
-		return false
+		allow = false
+		return
 	}
+	cmdList = append(cmdList, path)
 	switch path {
 	case
 		"/usr/bin/echo",
@@ -59,13 +66,15 @@ func checkCmd(cmd string) bool {
 		"/usr/bin/x2goterminate-session",
 		"/usr/bin/x2goumount-session",
 		"/usr/bin/x2goversion":
-		return true
-	case "scp":
+		allow = true
+		return
+	case "/usr/bin/scp":
 		path := ""
 		for i := 1; i < len(args); i++ {
 			if args[i] != "-t" && args[i] != "-r" {
 				if path != "" {
-					return false
+					allow = false
+					return
 				}
 				path = args[i]
 			}
@@ -74,19 +83,53 @@ func checkCmd(cmd string) bool {
 			path = home + "/" + path
 			dirs, err := os.ReadDir(path)
 			if err != nil {
-				return true
+				allow = true
+				return
 			}
-			return len(dirs) <= 0
+			if len(dirs) >= 10 {
+				allow = false
+				return
+			}
+			for _, dir := range dirs {
+				var maxSize int
+				if dir.IsDir() || (dir.Type()&os.ModeSymlink) != 0 {
+					allow = false
+					return
+				}
+				fileName := dir.Name()
+				if !strings.HasPrefix(fileName, "key.") {
+					allow = false
+					return
+				}
+				if strings.HasSuffix(fileName, ".ident") {
+					maxSize = 1000
+				} else {
+					maxSize = 4000
+				}
+				stat, err := os.Stat(path + "/" + dir.Name())
+				if err != nil {
+					allow = false
+					return
+				}
+				size := stat.Size()
+				if size > int64(maxSize) {
+					allow = false
+					return
+				}
+			}
+			allow = true
+			return
 		} else {
 			if !strings.HasPrefix(path, ".x2go/") {
-				return false
+				allow = false
+				return
 			}
 			path = home + "/" + path
 			dirs, err := os.ReadDir(path)
 			if err != nil {
-				return true
+				allow = true
+				return
 			}
-			f := true
 			for _, dir := range dirs {
 				var maxSize = 0
 				switch dir.Name() {
@@ -103,44 +146,56 @@ func checkCmd(cmd string) bool {
 				case "session.log":
 					file, err := os.OpenFile(path+"/"+dir.Name(), os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0777)
 					if err != nil {
-						f = false
+						allow = false
+						return
 					}
 					file.Close()
 					maxSize = 1
+				case ".pulse-client.conf":
+					maxSize = 1000
+				case ".pulse-cookie":
+					maxSize = 1000
 				default:
 					maxSize = 0
 				}
 				stat, err := os.Stat(path + "/" + dir.Name())
 				if err != nil {
-					f = false
+					allow = false
+					return
 				}
 				size := stat.Size()
 				if size > int64(maxSize) {
-					f = false
+					allow = false
+					return
 				}
 			}
-			return f
+			allow = true
+			return
 		}
-	case "setsid":
+	case "/usr/bin/setsid":
 		return checkCmd(args[1])
-	case "bash":
+	case "/usr/bin/bash":
 		var script = ""
 		for i := 1; i < len(args); i++ {
 			if args[i] != "-l" && args[i] != "-c" {
 				if script != "" {
-					return false
+					allow = false
+					return
 				}
 				script = args[i]
 			}
 		}
-		var f = true
+		allow = true
 		cmds := parseCmd(script)
 		for _, cmd := range cmds {
-			f = f && checkCmd(cmd)
+			chCmdList, chCmdAllow := checkCmd(cmd)
+			cmdList = append(cmdList, chCmdList...)
+			allow = allow && chCmdAllow
 		}
-		return f
+		return
 	default:
-		return false
+		allow = false
+		return
 	}
 }
 
@@ -154,6 +209,12 @@ func parseCmd(script string) []string {
 		cmds = append(cmds, script[st:ed])
 	}
 	return cmds
+}
+
+func setSelfPriority(priority int) {
+	pid := os.Getpid()
+	cmd := exec.Command("sudo", "renice", "-n", strconv.Itoa(priority), "-p", strconv.Itoa(pid))
+	cmd.Run()
 }
 
 func main() {
@@ -171,7 +232,21 @@ func main() {
 		return
 	}
 	// Temporarily release ssh&&scp
-	if len(cmd) == 1 && checkCmd(cmd[0]) {
-		syscall.Exec("/bin/bash", []string{"bash", "-c", sshOriginalCmd}, os.Environ())
+	if len(cmd) > 1 {
+		journal.Print(journal.PriNotice, "Incorrect cmd: %v\n", sshOriginalCmd)
+	} else {
+		cmdList, allow := checkCmd(cmd[0])
+		if allow {
+			prio := -11
+			for _, cmd := range cmdList {
+				if cmd == "/usr/bin/x2goruncommand" {
+					prio = 0
+				}
+			}
+			setSelfPriority(prio)
+			syscall.Exec("/bin/bash", []string{"bash", "-c", sshOriginalCmd}, os.Environ())
+		} else {
+			journal.Print(journal.PriNotice, "Reject cmd: %v\n", sshOriginalCmd)
+		}
 	}
 }
--- forceCmd/go.mod
+++ forceCmd/go.mod
@@ -3,6 +3,7 @@ module forceCmd
 go 1.23.0
 
 require (
+	github.com/coreos/go-systemd/v22 v22.5.0
 	github.com/mattn/go-shellwords v1.0.12
 	mvdan.cc/sh/v3 v3.9.0
 )
