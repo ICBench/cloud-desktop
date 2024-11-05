d91b17b5218f56e646ad4cfc81ed6bde14acf4a0
YOUR_NAME_HERE <YOUR_EMAIL_ADDRESS_HERE>
2024-11-05 14:31:08 +0800

    Add local oss support

--- datapath/server/server.go
+++ datapath/server/server.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"crypto/ed25519"
+	"crypto/tls"
 	"crypto/x509"
 	"database/sql"
 	"encoding/hex"
 	"encoding/json"
 	"encoding/pem"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -14,13 +18,19 @@ import (
 
 	"github.com/coreos/go-systemd/v22/journal"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 var (
-	crtFilePath = "./server.crt"
-	keyFilePath = "./server.key"
-	pubKeys     = []ed25519.PublicKey{}
-	db          *sql.DB
+	crtFilePath  = "./server.crt"
+	keyFilePath  = "./server.key"
+	pubKeys      = []ed25519.PublicKey{}
+	dbClient     *sql.DB
+	ossClient    *minio.Client
+	ossAccessKey = "yX61qRAWhaHcQPEXwYcQ"
+	ossSecretKey = "cUzNE7CuCy53DZMtMP667ahn2Nz7eDPdJoBcUQtQ"
+	ossEndpoint  = "127.0.0.1:9000"
 )
 
 type appFile struct {
@@ -30,20 +40,40 @@ type appFile struct {
 
 func connectDb() {
 	var err error
-	db, err = sql.Open("mysql", "shizhao:icbench@tcp(127.0.0.1:3306)/data_path_server")
+	dbClient, err = sql.Open("mysql", "shizhao:icbench@tcp(127.0.0.1:3306)/data_path_server")
 	if err != nil {
 		journal.Print(journal.PriErr, "Failed to connect database: %v\n", err)
 		os.Exit(-1)
 	}
-	err = db.Ping()
+	err = dbClient.Ping()
 	if err != nil {
 		journal.Print(journal.PriErr, "Failed to connect database: %v\n", err)
 		os.Exit(-1)
 	}
 }
 
+func connectOss() {
+	var err error
+	certPool := x509.NewCertPool()
+	certBytes := []byte("-----BEGIN CERTIFICATE-----\nMIIB4DCCAYagAwIBAgIQfaQTy1UvE2nSmVdcBt6x+DAKBggqhkjOPQQDAjA6MRww\nGgYDVQQKExNDZXJ0Z2VuIERldmVsb3BtZW50MRowGAYDVQQLDBFyb290QHh1YnVu\ndHUyNC4wNDAeFw0yNDExMDUwNjAzMThaFw0yNTExMDUwNjAzMThaMDoxHDAaBgNV\nBAoTE0NlcnRnZW4gRGV2ZWxvcG1lbnQxGjAYBgNVBAsMEXJvb3RAeHVidW50dTI0\nLjA0MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEt2f2CQobGLrpG9xWCCvjlfJQ\nefnwQmnEs8mnaCTC5QeAqqRz8dN9CyoFktnT76U11yhW04wHBk+g/9CDUucAG6Nu\nMGwwDgYDVR0PAQH/BAQDAgKkMBMGA1UdJQQMMAoGCCsGAQUFBwMBMA8GA1UdEwEB\n/wQFMAMBAf8wHQYDVR0OBBYEFLfADvpqY/Mb+uetjzilEO6bzeZQMBUGA1UdEQQO\nMAyHBH8AAAGHBGoP7EEwCgYIKoZIzj0EAwIDSAAwRQIhAOVDtvm6T0iu8CfMgPiN\nAtlBwc+qteQ4qKRv8rCk2NJTAiAeVBJJoxXPL/EvyEtVFSUYd+qgvh/ri6cJRBVV\noFmAZA==\n-----END CERTIFICATE-----")
+	certPool.AppendCertsFromPEM(certBytes)
+	ossClient, err = minio.New(ossEndpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(ossAccessKey, ossSecretKey, ""),
+		Secure: true,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs: certPool,
+			},
+		},
+	})
+	if err != nil {
+		journal.Print(journal.PriErr, "Failed to connect OSS: %v\n", err)
+		os.Exit(-1)
+	}
+}
+
 func loadPubKeys() {
-	keyCows, err := db.Query("SELECT public_key FROM users")
+	keyCows, err := dbClient.Query("SELECT public_key FROM users")
 	if err != nil {
 		journal.Print(journal.PriErr, "Failed to get pubkey from database: %v\n", err)
 		return
@@ -63,7 +93,7 @@ func loadPubKeys() {
 }
 
 func checkFileExist(hash string) bool {
-	rows, err := db.Query("SELECT hash FROM files WHERE hash = ?", hash)
+	rows, err := dbClient.Query("SELECT hash FROM files WHERE hash = ?", hash)
 	if err != nil {
 		journal.Print(journal.PriErr, "Failed to query file info from database: %v\n", err)
 		return false
@@ -71,14 +101,14 @@ func checkFileExist(hash string) bool {
 	return rows.Next()
 }
 
-func createNewApplication(userKey ed25519.PublicKey, sendFileList []appFile) {
+func createNewApplication(userKey ed25519.PublicKey, sendFileList []appFile, status int16) {
 	pubKeyBytes, err := x509.MarshalPKIXPublicKey(userKey)
 	if err != nil {
 		journal.Print(journal.PriErr, "Failed to marshal public key: %v", err)
 		return
 	}
 	user := string(pem.EncodeToMemory(&pem.Block{Type: "PUBLIC KEY", Bytes: pubKeyBytes}))
-	result, err := db.Exec("INSERT INTO applications (owner) VALUES (?)", user)
+	result, err := dbClient.Exec("INSERT INTO applications (owner,approval_status) VALUES (?,?)", user, status)
 	if err != nil {
 		journal.Print(journal.PriErr, "Failed to create new application: %v", err)
 		return
@@ -89,7 +119,7 @@ func createNewApplication(userKey ed25519.PublicKey, sendFileList []appFile) {
 		return
 	}
 	for _, sendFile := range sendFileList {
-		_, err := db.Exec("INSERT INTO application_file (file_hash,application_id,file_info) VALUES (?,?,?)", sendFile.Hash, id, sendFile.RelPath)
+		_, err := dbClient.Exec("INSERT INTO application_file (file_hash,application_id,file_info) VALUES (?,?,?)", sendFile.Hash, id, sendFile.RelPath)
 		if err != nil {
 			journal.Print(journal.PriErr, "Failed to create new application: %v", err)
 			return
@@ -98,25 +128,27 @@ func createNewApplication(userKey ed25519.PublicKey, sendFileList []appFile) {
 }
 
 func saveFile(fileBytes []byte, hash string) error {
-	savePath := "./tmp/" + hash
-	os.MkdirAll("./tmp", 0755)
-	saveFile, err := os.Create(savePath)
+	ctx := context.Background()
+	_, err := ossClient.PutObject(ctx, "files", hash, bytes.NewReader(fileBytes), int64(len(fileBytes)), minio.PutObjectOptions{})
 	if err != nil {
-		journal.Print(journal.PriErr, "Create file error: %v\n", err)
-		return err
-	}
-	_, err = saveFile.Write(fileBytes)
-	if err != nil {
-		journal.Print(journal.PriErr, "Write file error: %v\n", err)
+		journal.Print(journal.PriErr, "Save file to oss error: %v\n", err)
 		return err
 	}
 	expTime := time.Now().AddDate(0, 30, 0)
-	_, err = db.Exec("INSERT INTO files (hash,expiration_time) VALUES (?,?)", hash, expTime)
+	_, err = dbClient.Exec("INSERT INTO files (hash,expiration_time) VALUES (?,?)", hash, expTime)
 	if err != nil {
 		journal.Print(journal.PriErr, "Update file database error: %v\n", err)
 		return err
 	}
 	return nil
+}
+
+func delFile(hash string) {
+	ctx := context.Background()
+	err := ossClient.RemoveObject(ctx, "files", hash, minio.RemoveObjectOptions{})
+	if err != nil {
+		journal.Print(journal.PriErr, "Delete file in oss error: %v\n", err)
+	}
 }
 
 func checkSign(dataBytes []byte, signature []byte) ed25519.PublicKey {
@@ -128,9 +160,14 @@ func checkSign(dataBytes []byte, signature []byte) ed25519.PublicKey {
 	return nil
 }
 
+func isIntranet(addr string) bool {
+	return addr == ""
+}
+
 func uploadServer() error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/apply", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.RemoteAddr)
 		if r.Method != http.MethodPost {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
@@ -159,7 +196,13 @@ func uploadServer() error {
 			}
 		}
 		if len(needFileList) == 0 {
-			createNewApplication(user, sendFileList)
+			var iniStat int16
+			if isIntranet(r.RemoteAddr) {
+				iniStat = 0
+			} else {
+				iniStat = 1
+			}
+			createNewApplication(user, sendFileList, iniStat)
 		}
 		w.Header().Set("Content-Type", "application/json")
 		returnBytes, _ := json.Marshal(needFileList)
@@ -206,8 +249,41 @@ func downloadServer() {
 	http.ListenAndServeTLS("0.0.0.0:9991", crtFilePath, keyFilePath, mux)
 }
 
+func fileTidy() {
+	outdatedFileRow, err := dbClient.Query("SELECT hash FROM files WHERE expiration_time < ?", time.Now())
+	if err != nil {
+		journal.Print(journal.PriErr, "Error while trying to tidy files: %v\n", err)
+		return
+	}
+	var delReqList = make(map[int]struct{})
+	var delFileList = make(map[string]struct{})
+	for outdatedFileRow.Next() {
+		var hash string
+		outdatedFileRow.Scan(&hash)
+		delFile(hash)
+		delFileList[hash] = struct{}{}
+		outdatedReqRow, err := dbClient.Query("SELECT application_id FROM application_file WHERE file_hash = ?", hash)
+		if err != nil {
+			journal.Print(journal.PriErr, "Error while trying to tidy applications: %v\n", err)
+			return
+		}
+		for outdatedReqRow.Next() {
+			var id int
+			outdatedReqRow.Scan(&id)
+			delReqList[id] = struct{}{}
+		}
+	}
+	for req := range delReqList {
+		dbClient.Exec("DELETE FROM applications WHERE id = ?", req)
+	}
+	for file := range delFileList {
+		dbClient.Exec("DELETE FROM files WHERE hash = ?", file)
+	}
+}
+
 func main() {
 	connectDb()
+	connectOss()
 	loadPubKeys()
 	go func() {
 		for {
@@ -219,6 +295,7 @@ func main() {
 	}()
 	go downloadServer()
 	for {
-		time.Sleep(time.Hour)
+		fileTidy()
+		time.Sleep(24 * time.Hour)
 	}
 }
