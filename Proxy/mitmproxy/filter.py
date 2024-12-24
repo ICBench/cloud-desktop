f6366420c91f9edc264013ca6d62144a6a117f81
YOUR_NAME_HERE <YOUR_EMAIL_ADDRESS_HERE>
2024-12-24 14:08:17 +0800

    Allowed http request

--- Proxy/mitmproxy/filter.py
+++ Proxy/mitmproxy/filter.py
@@ -3,6 +3,7 @@ from tld import get_fld
 
 fldList=[
     "github.com",
+    "aliyuncs.com"
 ]
 
 def checkFld(fld: str)->bool:
@@ -12,26 +13,19 @@ def checkFld(fld: str)->bool:
     return False
 
 def request(flow: http.HTTPFlow)->None:
-    scheme=flow.request.scheme
-    host=flow.request.host
+    url=flow.request.url
+    fld=get_fld(url)
     contentType=flow.request.headers.get("content-type")
     method=flow.request.method
-    if scheme!="https":
-        flow.response = http.Response.make(
-            403,
-            b"Only HTTPS is available.",
-            {"Content-Type": "text/plain"}
-        )
     if method!="GET" and not (method=="POST" and contentType=="application/x-git-upload-pack-request"):
         flow.response = http.Response.make(
             403,
             b"Method is forbidden.",
             {"Content-Type": "text/plain"}
         )
-    fld=get_fld("https://"+host)
-    if checkFld(fld):
+    if not checkFld(fld):
         flow.response = http.Response.make(
             403,
-            f"{host} is forbidden.",
+            f"{url} is forbidden.",
             {"Content-Type": "text/plain"}
         )
